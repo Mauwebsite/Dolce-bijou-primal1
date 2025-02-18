cb89264b38c5df09c463feb3577fa78558f49567
Mauweb <mauricioarizaca@gmail.com>
2025-02-18 16:26:33 -0500

    second try

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  base: "https://Mauwebsite.github.io/Dolce-bijou-frontend.git"
+  // base: "https://Mauwebsite.github.io/Dolce-bijou-frontend.git"
+  base: "/Dolce-bijou-frontend/"
 })
