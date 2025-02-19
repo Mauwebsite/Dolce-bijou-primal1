b71ccbeaa3596fc906821e79dfca0d55a4366c11
Mauweb <mauricioarizaca@gmail.com>
2025-02-19 16:48:39 -0500

    commit fix routes 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate} from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -22,19 +22,19 @@ const App = () => {
       <ToastContainer />
       <Navbar />
       <SearchBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/collection' element={<Collection />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/product/:productId' element={<Product />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/place-order' element={<PlaceOrder />} />
-        <Route path='/orders' element={<Orders />} />
-        {/* <Route path="*" element={<Home />} />  */}
-        {/* <Route path="*" element={<Navigate to="/" />} /> */}
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/collection' element={<Collection />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/place-order' element={<PlaceOrder />} />
+          <Route path='/orders' element={<Orders />} />
+          {/* <Route path="*" element={<Home />} />  */}
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
+        </Routes>
       <Footer />
       <a href="https://api.whatsapp.com/send?phone=%2B5491162000494&text=Hola Siani, vengo de ver tu sitio web!" target="_blank">
 						<img src={assets.wspplogo} alt="Mi Whatsapp" className="fixed bottom-5 right-2.5 z-1000 w-[85px]" />
