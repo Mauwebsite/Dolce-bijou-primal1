b851f3e28a40e8cf0c34e705ebb196fa9ca54cff
Mauweb <mauricioarizaca@gmail.com>
2025-02-23 16:26:08 -0500

    commit 23-02-2025 | fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,11 @@ export default defineConfig({
   plugins: [react()],
   //server: { port: 5173 },
   // base: "https://Mauwebsite.github.io/Dolce-bijou-frontend.git"
-  base: "/Dolce-bijou-primal1/",
-  build: {
-    publicPath: "/Dolce-bijou-primal1/",
-  }
+  // base: "/Dolce-bijou-primal1/",
+  // build: {
+  //   publicPath: "/Dolce-bijou-primal1/",
+  // }
+  plugins: [react()],
+  base: '/', // Asegúrate de que esto esté aquí
 })
+
